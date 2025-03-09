a9370d0a326378e6d8bde183287ebbdca15b5674
aniketrastogi1987 <160590468+aniketrastogi1987@users.noreply.github.com>
2025-03-09 09:55:22 +0530

    Add files via upload

--- bikeshare_model/__init__.py
+++ bikeshare_model/__init__.py
@@ -8,4 +8,3 @@ sys.path.append(str(root))
 from bikeshare_model.config.core import PACKAGE_ROOT, config
 with open(PACKAGE_ROOT / "VERSION") as version_file:
     __version__ = version_file.read().strip()
-    
