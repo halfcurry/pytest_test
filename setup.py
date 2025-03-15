df294aad06fac5f8183910ba3248fd53957449ca
chaitrahs <chaitrahs@gmail.com>
2025-03-15 09:37:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
 
-from pathlib import Path
-from setuptools import find_packages, setup
-
-# Package meta-data.
-NAME = 'bikeshare_model'
-DESCRIPTION = "Bike sharing prediction model package"
-URL = "https://github.com/yourusername/bikeshare_app"
-EMAIL = "your.email@example.com"
-AUTHOR = "Your Name"
-REQUIRES_PYTHON = ">=3.7.0"
-
-# The rest of the code is below
-def list_reqs(fname="requirements/requirements.txt"):
-    with open(fname) as fd:
-        return fd.read().splitlines()
-
-
-# Load the package's VERSION file as a dictionary.
-about = {}
-ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / NAME
-with open(PACKAGE_DIR / "VERSION") as f:
-    _version = f.read().strip()
-    about["__version__"] = _version
-
+def read_requirements():
+    req_file = os.path.join(os.path.dirname(__file__), "requirements/requirements.txt")
+    if os.path.exists(req_file):
+        with open(req_file) as f:
+            return f.read().splitlines()
+    return []
 
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=("tests",)),
-    package_data={"bikeshare_model": ["VERSION"]},
-    install_requires=list_reqs(),
-    extras_require={},
-    include_package_data=True,
-    license="MIT",
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
+    name="bikeshare_model",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=read_requirements(),
 )
-
